2f92a4470274d10767623b8b109a176d5bbfd8b0
Antoine Gaillot <manzagregine@gmail.com>
2018-01-17 01:11:12 +0100

    Disabling api feature

--- api/init.js
+++ api/init.js
@@ -1,8 +1,11 @@
 const router = require('./router')
 const notFound = require('./middlewares/not-found')
 const errorHandler = require('./middlewares/error-handler')
+const disabler = require('./middlewares/disabler.middleware')
 
 module.exports = ({ app }) => {
+    app.use(disabler)
+    
     app.use('/', router)
 
     // catch 404 and forward to error handler
--- config/config.js
+++ config/config.js
@@ -1,12 +1,13 @@
 const env = process.env.NODE_ENV || 'development'
 
-// eslint-disable-next-line prefer-template
+// eslint-disable-next-line prefer-template,import/no-dynamic-require
 const vars = require('./vars.' + env)
 
 module.exports = {
     env,
     logs: env === 'production' ? 'combined' : 'dev',
     port: process.env.PORT || 3000,
+    enableApi: process.env.ENABLE_API || false,
     dailydadjokes: {
         baseUrl: 'https://icanhazdadjoke.com',
     },
